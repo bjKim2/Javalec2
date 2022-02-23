8f826a9957f1339f478dbfcab65917883cee0412
bjKim2 <rlaxodjs3@naver.com>
2022-02-23 17:26:13 +0900

    test11

--- For/src/com/javalec/base/Main.java
+++ For/src/com/javalec/base/Main.java
@@ -5,13 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// 1부터 10까지의 수중 홀수만 출력하
+		// 1부터 10까지의 수중 홀수만 출력하기
+		int oddsum = 0;
+		int evensum = 0;
 		
-		for(int i= 1; i<=10 ; i+=2 ) {
-			System.out.println(i);
+		
+		for(int i= 1; i<=100 ; i++ ) {
+			switch(i%2) {
+			case 0:
+				evensum +=i;
+				break;
+			default:
+				oddsum +=i;
+				break;
+			}
 		}
 		
-		
+		System.out.println("1부터 100까지 짝수의 합은 " + evensum + "이고 홀수의 합은 " + oddsum + "입니다.");
 	}
 
 }
