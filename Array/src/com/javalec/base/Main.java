efb39feada457b05dd40bafbed5e6905d6ee0ce4
bjKim2 <rlaxodjs3@naver.com>
2022-02-25 10:45:03 +0900

    for rebase

--- Array/src/com/javalec/base/Main.java
+++ Array/src/com/javalec/base/Main.java
@@ -4,7 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String[] str = new String[7];
+		
+		str[0] = "일요일";
+		str[1] = "월요일";
+		str[2] = "화요일";
+		str[3] = "수요일";
+		str[4] = "목요일";
+		str[5] = "금요일";
+		str[6] = "토요일";
+				
+		for(int i=1 ; i<str.length ; i+=2) {
+			System.out.println(str[i]);
+		}
+		
 	}
 
 }
