08e849e9cf717c5d13c3dca9253119e64b049d53
bjKim2 <rlaxodjs3@naver.com>
2022-02-25 11:17:29 +0900

    middle commit 02

--- Array/src/com/javalec/base/Main.java
+++ Array/src/com/javalec/base/Main.java
@@ -19,6 +19,20 @@ public class Main {
 			System.out.println(str[i]);
 		}
 		
+		String[] str1 = {"sun", "mon", "tue", "wed", "thr", "fri", "sat"};
+		int[] intNum = {1, 2, 3};
+		double[] doubleNum = {1.1 , 1.2};
+		boolean[] boolNum = {true, false, true};
+		
+		int[] intNum2 = {1,5,8,12,200,1002};
+		int sum=0;
+		double avg=0;
+		for(int i = 0; i < intNum2.length ; i++) {
+			sum += intNum2[i];
+			avg = (sum / intNum2.length) * 1.0;
+		
+		}
+		System.out.println(avg);
 	}
 
 }
