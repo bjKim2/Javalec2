c1cb1501c52e01009ed0d8c37568e8ca32cf27b5
TJ <rlaxodjs3@naver.com>
2022-02-25 11:43:42 +0900

    middle commit04

--- Array01/src/com/javalec/base/Main.java
+++ Array01/src/com/javalec/base/Main.java
@@ -7,13 +7,14 @@ public class Main {
 		
 		int[] intNum1 = {5,3,9};
 		
-		for(int i = 1 ; i<=9; i++) {
-			System.out.print(intNum1[0] + " X " + i +" = " + i * intNum1[0] +	"\t");
-			System.out.print(intNum1[1] + " X " + i +" = " + i * intNum1[1] +	"\t");
-			System.out.print(intNum1[2] + " X " + i +" = " + i * intNum1[2] +	"\t");
+		for(int j = 0 ; j <=2 ; j++) {
+			
+			for(int i = 1 ; i<=9; i++) {
+				System.out.print(intNum1[j] + " X " + i +" = " + i * intNum1[j] +	"\t");
+				
+			}
 			System.out.println();
 		}
-
 	}
 
 }
